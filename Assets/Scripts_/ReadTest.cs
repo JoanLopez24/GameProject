8f00fe68bc2ecb8c3909bf28bffcd19a1c7b9b75
Joan Lopez <joan.lopez.salvans@gmail.com>
2023-12-14 13:05:01 +0100

    SP1-GP-H01-T004-ReadSavedIDList(App)

--- Assets/Scripts_/ReadTest.cs
+++ Assets/Scripts_/ReadTest.cs
@@ -10,27 +10,46 @@ using Object = UnityEngine.Object;
 public class ReadTest : MonoBehaviour
 {
     private DatabaseReference reference;
+    List<int> values = new List<int>();
     // Start is called before the first frame update
     void Start()
     {
         // Get the root reference location of the database.
         reference = FirebaseDatabase.DefaultInstance.RootReference;
         FirebaseDatabase.DefaultInstance
-            .GetReference("users")
-            .GetValueAsync().ContinueWithOnMainThread(task => {
+            .GetReference("idList")
+            .OrderByKey()
+            .GetValueAsync().ContinueWith(task => {
                 if (task.IsFaulted) {
                     // Handle the error...
                 }
                 else if (task.IsCompleted) {
                     DataSnapshot snapshot = task.Result;
-                    // Do something with snapshot...
-                    foreach (DataSnapshot data in snapshot.Children)
-                    {
-                        Debug.Log(data.Child("username"));
+                    // Do something with snapshot..
+                    
+                    string info = "";
+                    // Mientras la lista no tenga la misma cantidad de elementos que el snapshot
+                    while (values.Count < snapshot.ChildrenCount) {
+                        // Itera a través de los hijos del snapshot
+                        foreach (DataSnapshot childSnapshot in snapshot.Children) {
+                            // Si la lista no contiene el valor y la clave es igual al tamaño de la lista
+                            if (!values.Contains(int.Parse(childSnapshot.Value.ToString())) && int.Parse(childSnapshot.Key) == values.Count) {
+                                // Añade el valor a la lista
+                                values.Add(int.Parse(childSnapshot.Value.ToString()));
+                                break;
+                            }
+                        }
                     }
-                   // Debug.Log("DATA RETRIEVED: ", username);
+
+                    foreach (int value in values)
+                    {
+                        Debug.Log(value);
+                    }
+                  
                 }
             });
+        
+        
     }
 
     // Update is called once per frame

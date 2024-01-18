4da8be780ec662dd91032a6d62662cd0b4bea778
Joan Lopez <joan.lopez.salvans@gmail.com>
2024-01-18 10:32:09 +0100

    Update SP1-GP-H04-T001-Spawn3DModelsApp
    Set RGBA Color from attribute

--- Assets/Scripts_/ReadTest.cs
+++ Assets/Scripts_/ReadTest.cs
@@ -13,10 +13,14 @@ public class ReadTest : MonoBehaviour
     private DatabaseReference reference;
     private List<PrefabState> prefabs = new List<PrefabState>();
     private GameObject[] gadgets;
+    public GameObject button;
     string info = "";
+
+    private bool dataIsLoaded;
     // Start is called before the first frame update
     void Start()
     {
+        dataIsLoaded = false;
         // Get the root reference location of the database.
         reference = FirebaseDatabase.DefaultInstance.RootReference;
         reference.Child("prefabs")
@@ -45,22 +49,62 @@ public class ReadTest : MonoBehaviour
                                 break;
                             }
                         }
+                        
+                        
                     }
                     
                     Debug.Log(prefabs.Count);
+                    dataIsLoaded = true;
+                    //SpawnPrefab(prefabs);
 
-                    foreach (PrefabState ps in prefabs)
-                    {
-                        Debug.Log(ps.prefabID);
-                    }
+
                 }
             });
+       
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (dataIsLoaded)
+        {
+            dataIsLoaded = false;
+            SpawnPrefab(prefabs);
+        }
+    }
+
+    
+    void SpawnPrefab(List<PrefabState> prefabList)
+    {
+        Debug.Log("SpawnPrefab: "+prefabList);
+        GameObject instance = Instantiate(button);
+        Debug.Log("HE INSTANCIAT " + instance.tag);
         
+        // Encuentra el objeto Button en     el prefab instanciado
+        GameObject btn = instance.transform.Find("Grab/Button").gameObject;
+        
+        // Recuperar color del prefab
+
+        foreach (PrefabState ps in prefabList)
+        {
+            Debug.Log(ps.color);
+        }
+        
+        string rgba = prefabList[0].color;
+        
+        string[] rgbaComponents = rgba.Split(',');
+        
+        Debug.Log(rgbaComponents[0] + rgbaComponents[1] + rgbaComponents[2] + rgbaComponents[3]);
+        
+        float r = float.Parse(rgbaComponents[0]);
+        float g = float.Parse(rgbaComponents[1]);
+        float b = float.Parse(rgbaComponents[2]);
+        float a = float.Parse(rgbaComponents[3]);
+        Color color = new Color(r / 1000f, g / 1000f, b / 1000f, a / 1000f);
+
+        // Asigna el color aleatorio al MeshRenderer del botón
+        btn.GetComponent<MeshRenderer>().material.color = color;
+       
     }
 }
